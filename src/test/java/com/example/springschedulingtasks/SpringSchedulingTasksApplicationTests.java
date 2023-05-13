c8a0e9a09569de877aa3f347998dc28b7d932ee2
bluepeng9 <bluepeng9@gmail.com>
2023-05-13 10:18:20 +0900

    Initial commit

---
+++ src/test/java/com/example/springschedulingtasks/SpringSchedulingTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springschedulingtasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSchedulingTasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
